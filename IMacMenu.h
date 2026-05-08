22c8bed8ae00102cadccfc0ca9398e62cbd74916
whatev.indus <77seventy77@users.noreply.github.com>
2026-05-07 22:50:21 -0700

    Fix release workflow portability

--- IMacMenu.h
+++ IMacMenu.h
@@ -1,10 +1,17 @@
 #ifndef IMacMenu_h
 #define IMacMenu_h
 
+#include <QtGlobal>
+
 class QString;
 class QWidget;
 
+#ifdef Q_OS_MACOS
 void DisableMacServicesMenu();
 void ApplyMacCenteredWindowTitle(QWidget* pWidget, const QString & krqstrTitle);
+#else
+inline void DisableMacServicesMenu() {}
+inline void ApplyMacCenteredWindowTitle(QWidget*, const QString &) {}
+#endif
 
 #endif // IMacMenu_h
